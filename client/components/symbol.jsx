5561d0d763031302919bcefbe97176e68aa04e75
Jaro <jaro@nous.lt>
2015-10-11 13:47:51 +0300

    Show ‘game over’ message when there are all 3 lifes lost.

--- client/components/symbol.jsx
+++ client/components/symbol.jsx
@@ -79,12 +79,21 @@ SymbolScreen = React.createClass({
   },
 
   render() {
+    var symbol;
+    if (this.data.status.mistake < 3) {
+      symbol = <SymbolComponent size={200}
+                 r={this.getSize()}
+                 a={this.getDirection()*90}
+                 cx= {100} cy={100}  />
+    } else {
+      symbol = <div className='game-over'>
+        <h1>Game Over</h1>
+        <div className="text">Symbol size: {this.data.status.size}</div>
+      </div>
+    }
     return (
       <div className="show-symbol">
-        <SymbolComponent size={200}
-                         r={this.getSize()}
-                         a={this.getDirection()*90}
-                         cx= {100} cy={100}  />
+        {symbol}
       </div>
     )
   }
